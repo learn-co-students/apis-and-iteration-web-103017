03c09f8a506858d2eae2ad282cbcc06bbb57fa14
erickalexander <erickcamacho1@gmail.com>
2017-10-31 15:47:40 -0400

    minor change in CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,7 +3,7 @@ def welcome
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character or characters separated by ','"
   gets.chomp.split(", ")
   # use gets to capture the user's input. This method should return that input, downcased.
 end
