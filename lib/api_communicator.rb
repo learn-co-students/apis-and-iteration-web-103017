cebcdef13e9b2bdd30c5f16ee21ba485712b3214
crutell331 <tcw331@gmail.com>
2017-10-31 16:20:26 -0400

    FINALLY finished the assignment for real this time. Can parse multiple pages of JSON.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,15 +5,35 @@ require 'pry'
 def get_character_movies_from_api(character)
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  character_hash["results"].collect do |char_prof|
-    if char_prof["name"] == character
-      return char_prof["films"].collect {|filmURL| JSON.parse(RestClient.get(filmURL))}
+  while character_hash
+    character_hash["results"].find do |char_prof|
+      if char_prof["name"].downcase == character.downcase
+        return char_prof["films"].collect {|filmURL| JSON.parse(RestClient.get(filmURL))}
+      end
+    end
+    if next?(character_hash)
+      character_hash = JSON.parse(RestClient.get(character_hash["next"]))
+    else
+      break
     end
   end
 end
 
+# Check if the "next" attribute in character_hash is null 
+def next?(character_hash)
+  if character_hash["next"].class != String
+    puts "Do you even Star Wars?"
+    false
+  else
+    true
+  end
+end
+
+
 def parse_character_movies(films_hash)
-  films_hash.each.with_index {|film, index| puts "#{index + 1}. #{film["title"]}"}
+  if films_hash.class == Array
+    films_hash.each.with_index {|film, index| puts "#{index + 1}. #{film["title"]}"}
+  end
 end
 
 def show_character_movies(character)
