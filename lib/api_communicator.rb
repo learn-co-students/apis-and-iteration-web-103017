ba783a2acba3bc5b7497643a773f5f3b63ef213d
c0nniewang <connie.wang303@gmail.com>
2017-10-31 15:44:14 -0400

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,20 +6,40 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  # INITIAL SOLUTION 
+  #result_arr = []
+  #character_hash["results"].each do |char|
+  #  if char["name"] == character 
+  #    char["films"].each do |url|
+  #      result = JSON.parse(RestClient.get(url))
+  #      result_arr << {result["title"] => result}
+  #    end
+  #  end
+  #end
+
+  # POSSIBLY BETTER SOLUTION
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  char = character_hash["results"]
+
+  result_arr = char.select do |item|
+    item["name"] == character
+  end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  result_arr[0]["films"].map do |url|
+    result = JSON.parse(RestClient.get(url))
+    {result["title"] => result}
+  end  
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+   films_hash.each.with_index do |hsh, idx|
+     puts "#{idx+1} #{hsh.keys.first}"
+   end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hi!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character:"
   # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
+  return input
 end
