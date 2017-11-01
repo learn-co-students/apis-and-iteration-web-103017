a8a91eb06e1e7a60f4e474158e7ee9030510cf31
bmcilhen <bmcilhen@gmail.com>
2017-11-01 09:44:14 -0400

    added .downcase to gets.chomp
    added .downcase to gets.chomp as the directions state you to do

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,6 +4,6 @@ end
 
 def get_character_from_user
   puts "Please enter a character."
-  character = gets.chomp
+  character = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
