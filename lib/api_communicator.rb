6165a43b606f0ed540ae51b772e26ab4e60fdd21
pawlkris <paulkristapovich@gmail.com>
2017-11-01 13:44:23 -0400

    all pages and cases of names

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,20 +6,25 @@ require_relative "../lib/command_line_interface.rb"
 
 
 def get_character_array(character)
-  film_apis = [] 
+  film_apis = []
   until film_apis != []
-    all_characters = RestClient.get('http://www.swapi.co/api/people/')
-    character_hash = JSON.parse(all_characters)
-    character_array = character_hash["results"]
+    # all_characters = RestClient.get('http://www.swapi.co/api/people/')
+    # character_hash = JSON.parse(all_characters)
+    # character_array = character_hash["results"]
+    until character_hash["next"] == nil
+      all_characters = RestClient.get(character_hash["next"])
+      character_hash = JSON.parse(all_characters)
+      character_array = character_array.concat character_hash["results"]
+    end
     character_array.each do |character_hash|
-      if character_hash["name"] == character
+      if character_hash["name"].downcase == character.downcase
         film_apis = character_hash["films"]
       end
     end
     if film_apis == []
       puts "We don't have information on that character, please try again."
       character = get_character_from_user
-    else 
+    else
       return film_apis
     end
   end
@@ -36,7 +41,7 @@ end
 def parse_character_movies(films_hash)
   films_hash.collect do |item|
     item["title"]
-  end 
+  end
 end
 
 def show_character_movies(character)
